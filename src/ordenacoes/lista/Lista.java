e3287619a31bc42b011cc38fdfcdfc9536a83ad0
RafaaelSF09 <rafaelfiori8@gmail.com>
2026-03-11 18:55:08 -0300

    Adicionadas as funções "Total", "NoIndice" e "ShellSort".

--- src/ordenacoes/lista/Lista.java
+++ src/ordenacoes/lista/Lista.java
@@ -65,7 +65,43 @@ public class Lista {
         }
     }
 
-    //ordenações
+    public void exibe(String texto)
+    {
+        No i = this.inicio;
+        System.out.println(texto);
+
+        while (i != null)
+        {
+            System.out.printf(i.getNum() + ", ");
+            i = i.getProx();
+        }
+        System.out.println(" ");
+    }
+
+    public int Total()
+    {
+        No aux = inicio;
+        int i = 0;
+        while (aux != null)
+        {
+            aux = aux.getProx();
+            i++;
+        }
+        return i;
+    }
+
+    public No NoIndice(int indice)
+    {
+        No aux = inicio;
+        while (aux != null && indice > 0)
+        {
+            aux = aux.getProx();
+            indice--;
+        }
+        return aux;
+    }
+
+    ///ordenações Mendes
     public void insercao_direta()
     {
         No pi = inicio.getProx(), ppos;
@@ -84,17 +120,39 @@ public class Lista {
         }
     }
 
-    public void exibe(String texto)
+    /// Ordenacoes Fiori
+    public void ShellSort()
     {
-        No i = this.inicio;
-        System.out.println(texto);
-
-        while (i != null)
+        int dist = 1;
+        int tl = Total();
+        int i,pos;
+        No aux1, aux2;
+        int auxNum;
+        while(dist < tl)
+            dist = 3 * dist + 1;
+        dist = dist/3;
+        while(dist > 0)
         {
-            System.out.printf(i.getNum() + ", ");
-            i = i.getProx();
+            i = dist;
+            pos = i;
+            while(i < tl)
+            {
+                aux1 = NoIndice(i);
+                auxNum = aux1.getNum();
+                aux2 = NoIndice(pos-dist);
+                while(pos > 0 && aux2 != null && auxNum < aux2.getNum())
+                {
+                    aux1.setNum(aux2.getNum());
+                    aux1 = aux2;
+                    pos = pos-dist;
+                    aux2 = NoIndice(pos);
+                }
+                aux1.setNum(auxNum);
+                i++;
+                aux1 = NoIndice(i);
+            }
+            dist = dist/3;
         }
-        System.out.println(" ");
     }
 
 }
